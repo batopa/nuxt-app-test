c3f806c7525053395d546b270cf24fbb1289235d
Alberto Pagliarini <batopa@gmail.com>
2025-11-05 08:50:24 +0100

    feat: add nuxt-bedita

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>This is a test page</h1>
+    <p v-if="isLogged">You are logged in!</p>
+    <p v-else>You are not logged in.</p>
+</template>
+
+<script lang="ts" setup>
+const { isLogged } = useBeditaAuth();
+</script>
